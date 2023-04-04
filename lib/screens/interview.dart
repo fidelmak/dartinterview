7258462a63e9a29270db6cabf6350809acfa5e46
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-04-04 18:12:18 +0100

    modified:   lib/screens/interview.dart

--- lib/screens/interview.dart
+++ lib/screens/interview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class interview extends StatefulWidget {
   const interview({super.key});
@@ -10,17 +11,41 @@ class interview extends StatefulWidget {
 class _interviewState extends State<interview> {
   @override
   Widget build(BuildContext context) {
+    final List images = [
+      'images/sus1.svg',
+      'images/sus2.svg',
+      'images/sus3.svg',
+    ];
+
     return Scaffold(
+      backgroundColor: Colors.blue,
       appBar: AppBar(
         title: Text('Choose a stack ', style: TextStyle(color: Colors.blue)),
         backgroundColor: Colors.white,
+        elevation: 0.0,
       ),
       body: Center(
-        child: TextButton(
-          child: Text('go back to interview questions '),
-          onPressed: () {
-            Navigator.pop(context, '/home');
-          },
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              child: PageView.builder(
+                itemCount: images.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return SvgPicture.asset(
+                    images[index],
+                    fit: BoxFit.contain,
+                  );
+                },
+              ),
+            ),
+            TextButton(
+              child: Text('go back to interview questions '),
+              onPressed: () {
+                Navigator.pop(context, '/home');
+              },
+            ),
+          ],
         ),
       ),
 
